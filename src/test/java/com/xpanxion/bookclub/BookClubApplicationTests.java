70a8aa235f40fb8d24f6117833719cec6d252c31
jordan <16jloudis@gmail.com>
2022-08-09 10:39:55 -0400

    Created Backend applicaton for bookclub app

---
+++ src/test/java/com/xpanxion/bookclub/BookClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpanxion.bookclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
